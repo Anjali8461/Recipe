9760e379c0730c820a22f759060b3bb9d4161adc
Anjali Rathore <anjalirathore@Anjalis-MacBook-Air.local>
2024-05-13 19:54:48 +0530

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-[80%] m-auto p-10'>
+      <h2 className=''>Features</h2>
+      <h1>OUR AWESOME FEATURE</h1>
+    </div>
+  )
+}
+
+export default Footer
